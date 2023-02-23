28b941756413cc2b88538aee65e6eb388d62b1e4
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-02-23 17:09:53 +1000

    Add files via upload

---
+++ ProstoeChisloConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProstoeChisloConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int N = int.Parse(Console.ReadLine());
+            int i = 2;
+
+            bool ProstoeChislo = false;
+            while (i <= N - 1);
+            {
+                if (N % i == 0)
+                ProstoeChislo = true;
+
+                i++;
+            }
+            if (ProstoeChislo)
+            {
+                Console.WriteLine("Число является простым");
+            }
+            else
+            {
+                Console.WriteLine("Число не является простым");
+            }
+            Console.ReadKey();
+        }
+    }
+    }
