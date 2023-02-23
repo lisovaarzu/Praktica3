5124f4deebc7a53cd7fdba4d40f99af751a0b5ac
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-02-23 17:09:04 +1000

    Add files via upload

---
+++ PrilogenieConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrilogenieConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+
+            {
+                Console.WriteLine("Введенное число является четным");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Введенное число нечетное");
+                Console.ReadLine();
+            }
+        }
+    }
+}
