507677fc55a4dc0216c9d281fb5cec2ca8ba8a54
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-02-25 18:56:07 +1000

    Add files via upload

---
+++ MinimalAlementConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalAlementConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длинну последовательности: ");
+            int i = Int32.Parse(Console.ReadLine()), min = int.MaxValue, N = -1;
+
+            for (; i > 0; i--)
+            {
+                N = Int32.Parse(Console.ReadLine());
+                min = min > N ? N : min;
+            }
+            Console.WriteLine("Наименьшее число последовательности: " + min);
+            Console.ReadKey(true);
+        }
+    }
+}
