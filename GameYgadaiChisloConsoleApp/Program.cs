28b5c3336082704db75378318077ec06c36694c6
Arzu <114538107+lisovaarzu@users.noreply.github.com>
2023-03-05 18:17:20 +1000

    Add files via upload

---
+++ GameYgadaiChisloConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameYgadaiChisloConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите конец интервала: ");
+            int y = int.Parse (Console.ReadLine());
+
+            Random rand = new Random();
+
+            int Number = rand.Next(0, y);
+            int count = 0;
+            int UserNumber;
+
+            Console.Write("Введите предполагаемое число или Enter для выхода: ");
+
+            do
+            {
+                var userInput = Console.ReadLine();
+                if (userInput == "")
+                {
+                    Console.Write ($"Загаданное число: {Number}.");
+                    Console.ReadKey();
+                    break;
+                }
+
+                count++;
+                UserNumber = Convert.ToInt32 (userInput);
+                
+                if (UserNumber < Number)
+                {
+                    Console.Write ("Введённое число меньше загаданного. Попробуйте снова: ");
+                }
+
+                else if (UserNumber > Number)
+                {
+                    Console.Write ("Введённое число больше загаданного. Попробуйте снова: ");
+                }
+
+                else
+                {
+                    Console.WriteLine ($"Введённое число угаданно. Попыток ввода {count}. ");
+                    Console.ReadKey();
+                    break;
+                }
+                
+            }
+            while (true);
+        }
+    }
+    
+}
+
+
